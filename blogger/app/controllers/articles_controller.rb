0ec6009f9adbf87d211629622c13cb92d4be05cd
Jerome Sardoma <Jerome@jeromes-mbp.fios-router.home>
2015-10-24 12:33:11 -0700

    form-based workflow feature completed

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
     
     def index
         @articles = Article.all #use '@' so that both controller and view can access the variable
@@ -6,6 +7,37 @@ class ArticlesController < ApplicationController
     
     def show
         @article = Article.find(params[:id])
+    end
+    
+    def new
+        @article = Article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def create
+        @article = Article.new(article_params)
+        @article.save #will not be in the database unless saved! 
+        flash.notice = "Article '#{@article.title}' created!"
+        redirect_to article_path(@article)
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        
+        flash.notice = "Article '#{@article.title}' updated!"
+        
+        redirect_to article_path(@article)
+    end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash.notice = "Article '#{@article.title}' deleted!"
+        redirect_to articles_path
     end
     
 end
